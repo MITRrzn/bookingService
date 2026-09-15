240d00c5f55aa88657b23f44b76bb840a0d1cebb
mitroshin.R <mitr.rzn@gmail.com>
2026-09-15 13:04:50 +0300

    structure event

--- internal/helper/responseHelper.go
+++ internal/helper/responseHelper.go
@@ -1,15 +1,18 @@
 package helper
 
 import (
-	"bookingService/internal/structs"
 	"encoding/json"
 	"net/http"
 )
 
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
 func WriteErrorResponse(w http.ResponseWriter, errorMessage string, statusCode int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
-	json.NewEncoder(w).Encode(structs.ErrorResponse{
+	json.NewEncoder(w).Encode(errorResponse{
 		Error: errorMessage,
 	})
 }
